f926b00957e18fe0f0a238e6d89d8af0ce6ddb1c
Gabriel Tan <gabriel.tanhk@gmail.com>
2023-11-20 14:18:40 +0800

    added questions and other pages

---
+++ eco-friendly app/ActualCalculatorView.swift
@@ -0,0 +1,8 @@
+//
+//  ActualCalculatorView.swift
+//  eco-friendly app
+//
+//  Created by Jonaven Tan on 20/11/23.
+//
+
+import Foundation
---
+++ eco-friendly app/ChallangeView.swift
@@ -0,0 +1,8 @@
+//
+//  ChallangeView.swift
+//  eco-friendly app
+//
+//  Created by Jonaven Tan on 20/11/23.
+//
+
+import Foundation
