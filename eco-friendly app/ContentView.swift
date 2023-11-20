7b62aa16353710f237c36c703f800248d9aa0f6f
Gabriel Tan <gabriel.tanhk@gmail.com>
2023-11-20 12:55:29 +0800

    home page

--- eco-friendly app/ContentView.swift
+++ eco-friendly app/ContentView.swift
@@ -7,15 +7,16 @@
 
 import SwiftUI
 
+import SwiftUI
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello world!")
+        TabView {
+            HomeView()
+            .tabItem {
+                Label("Home", systemImage: "house.fill")
+            }
         }
-        .padding()
     }
 }
 
--- eco-friendly app/Homepage.swift
+++ eco-friendly app/Homepage.swift
@@ -1,8 +1,37 @@
 //
-//  Homepage.swift
+//  HomeView.swift
 //  eco-friendly app
 //
-//  Created by Jonaven Tan on 20/11/23.
+//  Created by Yew Rei An on 20/11/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct HomeView: View {
+    
+    @AppStorage("bobuc") var bobuc = 0
+    @AppStorage("challengesCompleted") var challengesCompleted = 0
+    @AppStorage("lastCO2e") var lastCO2e = 0
+    
+    var body: some View {
+        VStack {
+            Text("Tip Of The Day: Use public transport.")
+                .font(.title3)
+                .bold()
+                .padding(15)
+            Text("Calculate your latest carbon emissions!")
+                .font(.title2)
+            Text("Last result: \(lastCO2e) kg CO2e")
+                .font(.title2)
+                .padding(5)
+            Text("Number of challenges completed: \(challengesCompleted)/8")
+                .font(.title2)
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
