9231eebf9c2c0b86edb4229152edafe8f5aa3c33
Govindarajan Nikhilesh <nikhilesh.govind@gmail.com>
2023-11-20 11:15:33 +0800

    Initial Commit

---
+++ eco-friendly app/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  eco-friendly app
+//
+//  Created by T Krobot on 20/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ eco-friendly app/eco_friendly_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  eco_friendly_appApp.swift
+//  eco-friendly app
+//
+//  Created by T Krobot on 20/11/23.
+//
+
+import SwiftUI
+
+@main
+struct eco_friendly_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
