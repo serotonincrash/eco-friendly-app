27d71072d57d1195ecfc0d65ab423d9378fcbd60
Random-Soldier-Dude <randomsoldierdude@gmail.com>
2023-11-21 09:03:02 +0800

    Fixed ActualCalculatorView.swift

--- eco-friendly app/ActualCalculatorView.swift
+++ eco-friendly app/ActualCalculatorView.swift
@@ -19,66 +19,68 @@ struct ActualCalculatorView: View {
     let options = ["Yes", "NO"]
     
     var body: some View {
-        Section("Yearly Gas Consumption") {
-            HStack {
-                TextField("Value from last year.", text: Binding(
-                    get: {"\(self.gasConsumption ?? 0)"},
-                    set: { self.gasConsumption = Int($0) ?? 0 }
-                ))
-                .multilineTextAlignment(.trailing)
-                Text("kilowatt-hour(s)")
-                    .foregroundColor(.secondary)
+        List {
+            Section("Yearly Gas Consumption") {
+                HStack {
+                    TextField("Value from last year.", text: Binding(
+                        get: {"\(self.gasConsumption ?? 0)"},
+                        set: { self.gasConsumption = Int($0) ?? 0 }
+                    ))
+                    .multilineTextAlignment(.trailing)
+                    Text("kilowatt-hour(s)")
+                        .foregroundColor(.secondary)
+                }
             }
-        }
-        Section("Yearly Water Consumption") {
-            HStack {
-                TextField("Value from last year.", text: Binding(
-                    get: {"\(self.waterConsumption ?? 0)"},
-                    set: { self.waterConsumption = Int($0) ?? 0 }
-                ))
-                .multilineTextAlignment(.trailing)
-                Text("cubic metre(s)")
-                    .foregroundColor(.secondary)
+            Section("Yearly Water Consumption") {
+                HStack {
+                    TextField("Value from last year.", text: Binding(
+                        get: {"\(self.waterConsumption ?? 0)"},
+                        set: { self.waterConsumption = Int($0) ?? 0 }
+                    ))
+                    .multilineTextAlignment(.trailing)
+                    Text("cubic metre(s)")
+                        .foregroundColor(.secondary)
+                }
             }
-        }
-        Section("Yearly Petrol Consumption") {
-            HStack {
-                TextField("Value from last year.", text: Binding(
-                    get: {"\(self.petrolConsumption ?? 0)"},
-                    set: { self.petrolConsumption = Int($0) ?? 0 }
-                ))
-                .multilineTextAlignment(.trailing)
-                Text("litre(s)")
-                    .foregroundColor(.secondary)
+            Section("Yearly Petrol Consumption") {
+                HStack {
+                    TextField("Value from last year.", text: Binding(
+                        get: {"\(self.petrolConsumption ?? 0)"},
+                        set: { self.petrolConsumption = Int($0) ?? 0 }
+                    ))
+                    .multilineTextAlignment(.trailing)
+                    Text("litre(s)")
+                        .foregroundColor(.secondary)
+                }
             }
-        }
-        Section("Time Travelling On Air") {
-            HStack {
-                TextField("Value from last year.", text: Binding(
-                    get: { "\(self.hourOnAircraft ?? 0)" },
-                    set: { self.hourOnAircraft = Int($0) ?? 0 }
-                ))
-                .multilineTextAlignment(.trailing)
-                Text("hour(s)")
-                    .foregroundColor(.secondary)
-                TextField("Value from last year.", text: Binding(
-                    get: { "\(self.minuteOnAircraft ?? 0)" },
-                    set: { self.minuteOnAircraft = Int($0) ?? 0 }
-                ))
-                .multilineTextAlignment(.trailing)
-                Text("minute(s)")
-                    .foregroundColor(.secondary)
+            Section("Time Travelling On Air") {
+                HStack {
+                    TextField("Value from last year.", text: Binding(
+                        get: { "\(self.hourOnAircraft ?? 0)" },
+                        set: { self.hourOnAircraft = Int($0) ?? 0 }
+                    ))
+                    .multilineTextAlignment(.trailing)
+                    Text("hour(s)")
+                        .foregroundColor(.secondary)
+                    TextField("Value from last year.", text: Binding(
+                        get: { "\(self.minuteOnAircraft ?? 0)" },
+                        set: { self.minuteOnAircraft = Int($0) ?? 0 }
+                    ))
+                    .multilineTextAlignment(.trailing)
+                    Text("minute(s)")
+                        .foregroundColor(.secondary)
+                }
+                if minuteOnAircraft == 60 {
+                    Text("Error: Minute(s) cannot exceed 60.")
+                        .foregroundColor(.red)
+                }
             }
-            if minuteOnAircraft == 60 {
-                Text("Error: Minute(s) cannot exceed 60.")
-                    .foregroundColor(.red)
-            }
-        }
-        Section("Submit Form") {
-            Button {
-                CarbonFootprintView()
-            } label: {
-                Text("Calculate your carbon footprint.")
+            Section("Submit Form") {
+                Button {
+                    CarbonFootprintView()
+                } label: {
+                    Text("Calculate your carbon footprint.")
+                }
             }
         }
     }
