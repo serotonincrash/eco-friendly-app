f595e18644c5e538ec18fb2f5b5851d5c390799b
Random-Soldier-Dude <randomsoldierdude@gmail.com>
2023-11-22 10:59:54 +0800

    did bingo

--- eco-friendly app/BingoView.swift
+++ eco-friendly app/BingoView.swift
@@ -11,6 +11,16 @@ struct BingoView: View {
     
     @State private var progressValue: Float = 0.5
     @State private var tappedIndex: Int?
+    @AppStorage("challenge1completed") var challenge1completed = false
+    @AppStorage("challenge2completed") var challenge2completed = false
+    @AppStorage("challenge3completed") var challenge3completed = false
+    @AppStorage("challenge4completed") var challenge4completed = false
+    @AppStorage("challenge5completed") var challenge5completed = false
+    @AppStorage("challenge6completed") var challenge6completed = false
+    @AppStorage("challenge7completed") var challenge7completed = false
+    @AppStorage("challenge8completed") var challenge8completed = false
+    @AppStorage("challenge9completed") var challenge9completed = false
+    @AppStorage("challengesCompleted") var challengesCompleted = 0
     
     var body: some View {
         VStack {
@@ -33,30 +43,115 @@ struct BingoView: View {
             ZStack {
                 if tappedIndex == 0 {
                     VStack {
-                        Text("Emit at most 2,000 kilogrammes.")
-                        Text(" ")
+                        Text("Achieve 90% Waste Diversion Rate")
+                            .bold()
+                        Text("Strive to divert 90% of household waste from landfills through recycling, composting, and other sustainable practices.")
+                        Button {
+                            challenge1completed = true
+                            challengesCompleted += 1
+                        } label: {
+                            Text("Done!")
+                        }
                     }
                 } else if tappedIndex == 1 {
-                    Text("")
+                    VStack {
+                        Text("Reduce Single-Use Plastics by 50%")
+                            .bold()
+                        Text("Set a goal to cut personal consumption of single-use plastics in half, measured by the quantity of plastic products used or purchased.")
+                        Button {
+                            challenge2completed = true
+                            challengesCompleted += 1
+                        } label: {
+                            Text("Done!")
+                        }
+                    }
                 } else if tappedIndex == 2 {
-                    Text("")
+                    VStack {
+                        Text("Increase Recycling Participation to 95%")
+                            .bold()
+                        Text("Aim to have 95% participation in recycling initiatives within your household or community, quantified by the number of individuals actively recycling.")
+                        Button {
+                            challenge3completed = true
+                            challengesCompleted += 1
+                        } label: {
+                            Text("Done!")
+                        }
+                    }
                 } else if tappedIndex == 3 {
-                    Text("")
+                    VStack {
+                        Text("Compost 75% of Organic Waste")
+                            .bold()
+                        Text("Establish a composting system and target the diversion of 75% of organic waste from landfills.")
+                        Button {
+                            challenge4completed = true
+                            challengesCompleted += 1
+                        } label: {
+                            Text("Done!")
+                        }
+                    }
                 } else if tappedIndex == 4 {
-                    Text("")
+                    VStack {
+                        Text("Purchase 80% Recycled or Sustainable Products")
+                            .bold()
+                        Text("Quantify efforts to support environmentally friendly products by setting a goal to purchase 80% of goods made from recycled materials or produced sustainably.")
+                        Button {
+                            challenge5completed = true
+                            challengesCompleted += 1
+                        } label: {
+                            Text("Done!")
+                        }
+                    }
                 } else if tappedIndex == 5 {
-                    Text("")
+                    VStack {
+                        Text("Complete 10 Upcycling Projects Annually")
+                            .bold()
+                        Text("Set a numerical target for upcycling projects completed each year, focusing on creatively repurposing materials.")
+                        Button {
+                            challenge6completed = true
+                            challengesCompleted += 1
+                        } label: {
+                            Text("Done!")
+                        }
+                    }
                 } else if tappedIndex == 6 {
-                    Text("")
+                    VStack {
+                        Text("Reduce Water Usage by 20%")
+                            .bold()
+                        Text("Measure and reduce water consumption by 20%, using water-saving technologies and mindful usage.")
+                        Button {
+                            challenge7completed = true
+                            challengesCompleted += 1
+                        } label: {
+                            Text("Done!")
+                        }
+                    }
                 } else if tappedIndex == 7 {
-                    Text("")
+                    VStack {
+                        Text("Advocate to 100 Individuals Annually")
+                        bold()
+                        Text("Quantify the impact of educational advocacy by aiming to inform and influence 100 individuals each year about recycling and sustainable practices.")
+                        Button {
+                            challenge8completed = true
+                            challengesCompleted += 1
+                        } label: {
+                            Text("Done!")
+                        }
+                    }
                 } else if tappedIndex == 8 {
-                    Text("")
+                    VStack {
+                        Text("Cut Carbon Emissions by 15%")
+                            .bold()
+                        Text("Establish a numerical target to reduce personal carbon emissions by 15%, achieved through changes in transportation habits, energy use, and lifestyle choices.")
+                        Button {
+                            challenge9completed = true
+                            challengesCompleted += 1
+                        } label: {
+                            Text("Done!")
+                        }
+                    }
                 } else {
                     VStack {
                         Text("Please select one of the nine options given.")
-                        Text("If you're reading this you are a hacker 0_0")
-                            .opacity(0)
                     }
                 }
             }
