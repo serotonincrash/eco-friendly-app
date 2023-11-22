7bcbc4113cc0bf67a0a2befce03320f1c033dec2
Govindarajan Nikhilesh <nikhilesh.govind@gmail.com>
2023-11-22 09:07:14 +0800

    made the results appear below the calculator form

--- eco-friendly app/ActualCalculatorView.swift
+++ eco-friendly app/ActualCalculatorView.swift
@@ -1,27 +1,34 @@
-//
-//  CalculatorView.swift
-//  eco-friendly app
-//
-//  Created by Yew Rei An on 20/11/23.
-//
-
 import SwiftUI
 
 struct ActualCalculatorView: View {
-    @AppStorage("D") var date: Double = 0.0
-    @AppStorage("gasConsumption") var gasConsumption: Int?
-    @AppStorage("waterConsumption") var waterConsumption: Int?
-    @AppStorage("petrolConsumption") var petrolConsumption: Int?
-    @AppStorage("hourOnAircraft") var hourOnAircraft: Int?
-    @AppStorage("minuteOnAircraft") var minuteOnAircraft: Int?
+    @EnvironmentObject var footprintManager: FootprintManager
+    @AppStorage("gasConsumption") var gasConsumption: Double = 0.0
+    @AppStorage("waterConsumption") var waterConsumption: Double = 0.0
+    @AppStorage("petrolConsumption") var petrolConsumption: Double = 0.0
+    @AppStorage("hourOnAircraft") var hourOnAircraft: Double = 0.0
+    @AppStorage("minuteOnAircraft") var minuteOnAircraft: Double = 0.0
+    @AppStorage("totalcarvonfootprint") var totalCarbonFootprint: Double = 0.0
+    @AppStorage("lastCO2e") var lastCO2e = 0
+    @AppStorage("Utilities") var utilities: Double = 0.0
+    @State private var showResultView: Bool = false
+    private var gasCarbonFootprint: Double { Double(gasConsumption * 100);Double() }
+    private var waterCarbonFootprint: Double { waterConsumption * 50 }
+    private var petrolCarbonFootprint: Double { petrolConsumption * 200 }
+    private var airCarbonFootprint: Double { ((minuteOnAircraft / 60) + hourOnAircraft) * 90 }
+
+    private func calculateCarbonFootprint() {
+        totalCarbonFootprint = gasCarbonFootprint + waterCarbonFootprint + petrolCarbonFootprint + airCarbonFootprint
+        lastCO2e = Int(totalCarbonFootprint)
+        utilities = gasCarbonFootprint + waterCarbonFootprint + petrolCarbonFootprint
+    }
     
     var body: some View {
-        NavigationStack {
-            List {
+        NavigationView {
+            Form {
                 Section("Yearly Gas Consumption") {
                     HStack {
                         TextField("Value from last year.", text: Binding(
-                            get: {"\(self.gasConsumption ?? 0)"},
+                            get: { "\(self.gasConsumption ?? 0)" },
                             set: { self.gasConsumption = Int($0) ?? 0 }
                         ))
                         .multilineTextAlignment(.trailing)
@@ -29,10 +36,11 @@ struct ActualCalculatorView: View {
                             .foregroundColor(.secondary)
                     }
                 }
+                
                 Section("Yearly Water Consumption") {
                     HStack {
                         TextField("Value from last year.", text: Binding(
-                            get: {"\(self.waterConsumption ?? 0)"},
+                            get: { "\(self.waterConsumption ?? 0)" },
                             set: { self.waterConsumption = Int($0) ?? 0 }
                         ))
                         .multilineTextAlignment(.trailing)
@@ -40,10 +48,11 @@ struct ActualCalculatorView: View {
                             .foregroundColor(.secondary)
                     }
                 }
+                
                 Section("Yearly Petrol Consumption") {
                     HStack {
                         TextField("Value from last year.", text: Binding(
-                            get: {"\(self.petrolConsumption ?? 0)"},
+                            get: { "\(self.petrolConsumption ?? 0)" },
                             set: { self.petrolConsumption = Int($0) ?? 0 }
                         ))
                         .multilineTextAlignment(.trailing)
@@ -51,6 +60,7 @@ struct ActualCalculatorView: View {
                             .foregroundColor(.secondary)
                     }
                 }
+                
                 Section("Time Travelling On Air") {
                     HStack {
                         TextField("Value from last year.", text: Binding(
@@ -60,6 +70,7 @@ struct ActualCalculatorView: View {
                         .multilineTextAlignment(.trailing)
                         Text("hour(s)")
                             .foregroundColor(.secondary)
+                        
                         TextField("Value from last year.", text: Binding(
                             get: { "\(self.minuteOnAircraft ?? 0)" },
                             set: { self.minuteOnAircraft = Int($0) ?? 0 }
@@ -68,32 +79,31 @@ struct ActualCalculatorView: View {
                         Text("minute(s)")
                             .foregroundColor(.secondary)
                     }
-                    if minuteOnAircraft != nil {
-                        if minuteOnAircraft! >= 60 {
-                            Text("Error: Minute(s) cannot exceed 60.")
-                                .foregroundColor(.red)
-                        }
+                    
+                    if let minuteOnAircraft = minuteOnAircraft, minuteOnAircraft >= 60 {
+                        Text("Error: Minute(s) cannot exceed 60.")
+                            .foregroundColor(.red)
                     }
                 }
-                Section("Date") {
-                    HStack {
-                        TextField("Put it in DD/MM.", text: Binding(
-                            get: { "\(self.dateAndTime ?? 0)" },
-                            set: { self.dateAndTime = Double(Int($0) ?? 0) }
-                        ))
-                        .multilineTextAlignment(.trailing)
-                        
-                        
-                        NavigationLink("Calculate your carbon footprint.") {
-                            CarbonFootprintView()
-                        }
-                        .foregroundColor(.blue)
+                
+                Button("Calculate your carbon footprint.") {
+                    
+                    VStack {
+                        Text("Your total carbon footprint is \(String(format: "%.2f", totalCarbonFootprint))")
+                        Text("Your carbon footprint for air travel is \(String(format: "%.2f", airCarbonFootprint))")
+                        Text("Your carbon footprint for utilities is \(String(format: "%.2f", utilities))")
                     }
+                    Button("Save") {
+                        footprintManager.footprints.append(Footprint(totalCarbonFootprint: totalCarbonFootprint, airCarbonFootprint: airCarbonFootprint, utilities: utilities))
+                               }
+                               .buttonStyle(CustomButtonStyle())
+                           }
+                       }
                 }
+                .foregroundColor(.blue)
+                .frame(maxWidth: .infinity, alignment: .center)
             }
-        }
-    }
-}
+                }
 
 struct ActualCalculatorView_Previews: PreviewProvider {
     static var previews: some View {
