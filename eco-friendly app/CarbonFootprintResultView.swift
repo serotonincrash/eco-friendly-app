a589908743cb6e27d7ae532f49326b523da6058a
Gabriel Tan <gabriel.tanhk@gmail.com>
2023-11-22 10:02:40 +0800

    made button to go back to home

---
+++ eco-friendly app/CarbonFootprintResultView.swift
@@ -0,0 +1,18 @@
+//
+//  CarbonFootprintResultView.swift
+//  eco-friendly app
+//
+//  Created by Jonaven Tan on 22/11/23.
+//
+
+import SwiftUI
+
+struct CarbonFootprintResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CarbonFootprintResultView()
+}
